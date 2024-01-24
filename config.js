2182b9fd8fda90eb0253f6181f3174bc8df6bd18
Mateusz Serafin <mateusz97.serafin@gmail.com>
2024-01-24 13:06:32 +0100

    Adde login and register pages

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: "http://192.168.0.104:19035",
+  API_URL: "http://192.168.0.107:19035",
   // other configurations
   STRIPE_KEY:
     "pk_test_51ORfB9CVslDt3sXBnA5naMHNVkFLkJ7NO0b6ZOsxgPRvizzp2qtLHvgqwgTVIDU5uk5DHy5gKDD0lmBEz4lQ5WS100g3zcYTo0",
